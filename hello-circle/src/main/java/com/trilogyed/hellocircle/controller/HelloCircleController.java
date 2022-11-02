7431b2f0704dbb9779ae6a58f7c1233f30a1b6f9
stephanie corral <stephanie.corral8@gmail.com>
2022-11-02 12:45:36 -0700

    adding the circleci folder and the yml file

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -1,2 +1,13 @@
-package com.trilogyed.hellocircle.controller;public class HelloCircleController {
+package com.trilogyed.hellocircle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloCircleController {
+
+    @GetMapping(value = "/hello")
+    public String helloCircle(){
+        return "Hello, Circle!";
+    }
 }
